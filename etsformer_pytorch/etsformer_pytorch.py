8417254276e855aabf40d2c6746029c9cebebf15
Phil Wang <lucidrains@gmail.com>
2022-03-14 12:24:12 -0700

    complete naive slow MHESA

--- etsformer_pytorch/etsformer_pytorch.py
+++ etsformer_pytorch/etsformer_pytorch.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn.functional as F
 from torch import nn, einsum
 
-from einops import rearrange
+from einops import rearrange, repeat
 from einops.layers.torch import Rearrange
 
 # helper functions
@@ -29,14 +29,96 @@ def InputEmbedding(time_features, model_dim, kernel = 3, dropout = 0.):
         Rearrange('b d n -> b n d'),
     )
 
+class NaiveMHESA(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim,
+        heads = 8,
+        dim_head = 32,
+        dropout = 0.
+    ):
+        super().__init__()
+        inner_dim = heads * dim_head
+        self.heads = heads
+        self.initial_state = nn.Parameter(torch.randn(heads, dim_head))
+
+        self.dropout = nn.Dropout(dropout)
+        self.alpha = nn.Parameter(torch.randn(heads))
+
+        self.project_in = nn.Linear(dim, inner_dim)
+        self.project_out = nn.Linear(inner_dim, dim)
+
+    def forward(self, x):
+        # in appendix A.1 - Algorithm 2
+
+        b, n, d, h, device = *x.shape, self.heads, x.device
+
+        # linear project in
+
+        x = self.project_in(x)
+
+        # split out heads
+
+        x = rearrange(x, 'b n (h d) -> b h n d', h = h)
+
+        # temporal difference
+
+        x = torch.cat((
+            repeat(self.initial_state, 'h d -> b h 1 d', b = b),
+            x
+        ), dim = -2)
+
+        x = x[:, :, 1:] - x[:, :, :-1]
+
+        # prepare exponential alpha
+
+        alpha = self.alpha.sigmoid()
+        alpha = rearrange(alpha, 'h -> h 1')
+
+        # arange == powers
+
+        arange = torch.arange(n, device = device)
+        weights = alpha * (1 - alpha) ** torch.flip(arange, dims = (0,))
+
+        weights = repeat(weights, '... l -> ... t l', t = n)
+        indices = repeat(arange, 'l -> h t l', h = h, t = n)
+
+        indices = (indices - rearrange(arange + 1, 't -> 1 t 1')) % n
+
+        weights = weights.gather(-1, indices)
+        weights = self.dropout(weights)
+
+        # causal
+
+        weights = weights.tril()
+
+        # multiply
+
+        output = einsum('b h n d, h m n -> b h m d', x, weights)
+
+        # get initial state contribution
+
+        init_weight = (1 - alpha) ** (arange + 1)
+        init_output = rearrange(init_weight, 'h n -> h n 1') * rearrange(self.initial_state, 'h d -> h 1 d')
+
+        output = output + init_output
+
+        # merge heads
+
+        output = rearrange(output, 'b h n d -> b n (h d)')
+        return self.project_out(output)
+
 class FrequencyAttention(nn.Module):
     def __init__(
         self,
         *,
-        K = 4
+        K = 4,
+        dropout = 0.
     ):
         super().__init__()
         self.K = K
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         freqs = torch.fft.rfft(x, dim = 1)
@@ -44,6 +126,7 @@ class FrequencyAttention(nn.Module):
         # get amplitudes
 
         amp = freqs.abs()
+        amp = self.dropout(amp)
 
         # topk amplitudes - for seasonality, branded as attention
 
@@ -66,6 +149,7 @@ class ETSFormer(nn.Module):
         time_features,
         model_dim,
         heads = 8,
+        dim_head = 32,
         K = 4
     ):
         super().__init__()
